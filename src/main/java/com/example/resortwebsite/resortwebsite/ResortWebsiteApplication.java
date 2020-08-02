f01afe0eb89db2ed38e12aeaf70a28770f7180ff
anurag <anurag.adstacks@gmail.com>
2020-08-02 15:29:01 +0530

    add spring project

---
+++ src/main/java/com/example/resortwebsite/resortwebsite/ResortWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.resortwebsite.resortwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResortWebsiteApplication.class, args);
+	}
+
+}
