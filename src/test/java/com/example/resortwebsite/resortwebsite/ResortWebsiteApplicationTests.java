fbff8658de457326afed7bd98cab8646e9ffac2b
anurag <anurag.adstacks@gmail.com>
2020-08-02 15:32:40 +0530

    add spring boot project

---
+++ src/test/java/com/example/resortwebsite/resortwebsite/ResortWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resortwebsite.resortwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
